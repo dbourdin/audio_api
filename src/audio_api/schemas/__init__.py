0e169e2f44e7dcd6445fff0d1ff10a7d8c5b4a46
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-13 03:40:58 +0200

    Added program schemas.

--- src/audio_api/schemas/__init__.py
+++ src/audio_api/schemas/__init__.py
@@ -1,1 +1,12 @@
 from audio_api.schemas.api import ApiVersionModel
+from audio_api.schemas.base import APIMessage, APISchema
+from audio_api.schemas.program import (
+    ProgramCreateDB,
+    ProgramCreateIn,
+    ProgramCreateOut,
+    ProgramGet,
+    ProgramList,
+    ProgramUpdateDB,
+    ProgramUpdateIn,
+    ProgramUpdateOut,
+)
