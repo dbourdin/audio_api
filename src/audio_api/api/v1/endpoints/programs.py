447601823e98a68ed256fa95c5538401266cd654
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-13 03:52:07 +0200

    Add missing endpoints for programs.

--- src/audio_api/api/v1/endpoints/programs.py
+++ src/audio_api/api/v1/endpoints/programs.py
@@ -46,8 +46,8 @@ async def get(
 @router.get(
     "",
     response_model=list[schemas.ProgramList],
-    summary="List Users",
-    description="Get a list of Users",
+    summary="List Programs",
+    description="Get a list of Programs",
 )
 def retrieve_many(
     db: Session = Depends(deps.get_db),
@@ -73,3 +73,87 @@ def retrieve_many(
     ]
 
     return programs
+
+
+@router.post(
+    "",
+    response_model=schemas.ProgramCreateOut,
+    status_code=status.HTTP_201_CREATED,
+    responses={status.HTTP_400_BAD_REQUEST: {"model": schemas.APIMessage}},
+    summary="Create a Program",
+    description="Create a Program",
+)
+async def create(
+    *,
+    db: Session = Depends(deps.get_db),
+    program_in: schemas.ProgramCreateIn,
+) -> Any:
+    """Create a new program.
+
+    Args:
+        db (Session): A database session
+        program_in (schemas.ProgramCreateIn): Input data
+    """
+    db_program = schemas.ProgramCreateOut(
+        uuid=uuid.uuid4(),
+        title=program_in.title,
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+    )
+    return db_program
+
+
+@router.put(
+    "/{program_id}",
+    response_model=schemas.ProgramUpdateOut,
+    responses={
+        status.HTTP_403_FORBIDDEN: {"model": schemas.APIMessage},
+        status.HTTP_404_NOT_FOUND: {"model": schemas.APIMessage},
+    },
+    summary="Edit a Program",
+    description="Edit a Program",
+)
+async def update(
+    *,
+    db: Session = Depends(deps.get_db),
+    program_id: uuid.UUID,
+    program_in: schemas.ProgramUpdateIn,
+) -> Any:
+    """Update an existing Program.
+
+    Args:
+        db (Session): A database session
+        program_id (uuid.UUID): The uuid of the program to modify
+        program_in (schemas.ProgramUpdateIn): The new data
+    """
+    updated_instance = schemas.ProgramUpdateOut(
+        uuid=program_id,
+        title=program_in.title,
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+    )
+
+    return updated_instance
+
+
+@router.delete(
+    "/{program_id}",
+    responses={
+        status.HTTP_403_FORBIDDEN: {"model": schemas.APIMessage},
+        status.HTTP_404_NOT_FOUND: {"model": schemas.APIMessage},
+    },
+    summary="Delete a Program",
+    description="Delete a Program",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete(
+    *,
+    db: Session = Depends(deps.get_db),
+    program_id: uuid.UUID,
+) -> Any:
+    """Delete an existing Program.
+
+    Args:
+        db (Session): A database session
+        program_id (uuid.UUID): The uuid of the program to delete
+    """
