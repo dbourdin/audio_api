c2be75c8986460abe670f9b88bd92485c6e71cf3
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-14 08:09:15 +0200

    Add get_by_program_id to radio_program repository.

--- src/audio_api/persistence/repositories/radio_program.py
+++ src/audio_api/persistence/repositories/radio_program.py
@@ -1,5 +1,9 @@
 """RadioProgramRepository to handle DB persistence."""
 
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
 from audio_api.persistence.models.radio_program import RadioProgram
 from audio_api.persistence.repositories.base_repository import BaseRepository
 from audio_api.schemas import RadioProgramCreateDB, RadioProgramUpdateDB
@@ -9,6 +13,18 @@ class RadioProgramRepository(
     BaseRepository[RadioProgram, RadioProgramCreateDB, RadioProgramUpdateDB]
 ):
     """RadioProgramRepository to handle DB persistence."""
+
+    def get_by_program_id(self, db: Session, program_id: UUID) -> RadioProgram | None:
+        """Get a single radio program by program_id.
+
+        Args:
+            db (Session): A database session
+            program_id (UUID): The program_id to retrieve
+
+        Returns:
+            Optional[RadioProgram]: The retrieved radio program
+        """
+        return db.query(self.model).filter(self.model.program_id == program_id).first()
 
 
 radio_program = RadioProgramRepository(RadioProgram)
