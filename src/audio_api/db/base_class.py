576076422e8c9549b6dd835a06a4fdc32664d9fc
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-14 05:33:57 +0200

    Add base migration with RadioProgram.

--- src/audio_api/db/base_class.py
+++ src/audio_api/db/base_class.py
@@ -2,8 +2,7 @@
 
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Integer, text
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, DateTime, Integer
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.sql.functions import now
 
@@ -16,13 +15,6 @@ class Base:
     # Generate __tablename__ automatically
 
     id = Column(Integer, primary_key=True, index=True)
-    uuid = Column(
-        UUID(as_uuid=True),
-        index=True,
-        unique=True,
-        server_default=text("uuid_generate_v4()"),
-    )
-
     created_at = Column(DateTime(timezone=True), server_default=now())
     updated_at = Column(
         DateTime(timezone=True), server_default=now(), onupdate=datetime.now
