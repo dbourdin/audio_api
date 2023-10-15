628ba345ffe139e0132cea0d3c3d0aa4b9a11583
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-15 20:49:26 +0200

    Introduced boto3. Add settings for AWS S3 creds and bucket name. Introduced S3Connector and add S3ProgramFilePersistence.

---
+++ src/audio_api/s3/program_file_persistence.py
@@ -0,0 +1,42 @@
+"""ProgramFilePersistence class to persist RadioPrograms in S3."""
+
+import datetime
+
+from audio_api.s3.s3_connector import S3Connector
+from audio_api.schemas import RadioProgramCreateIn
+from audio_api.settings import get_settings
+
+settings = get_settings()
+
+
+class ProgramFilePersistence:
+    """Class used to persist RadioPrograms in RADIO_PROGRAMS_BUCKET."""
+
+    s3_connector = S3Connector(bucket_name=settings.RADIO_PROGRAMS_BUCKET)
+
+    @classmethod
+    def persist_program(
+        cls, radio_program: RadioProgramCreateIn, program_file: bytes
+    ) -> str:
+        """Persist a RadioProgram in RADIO_PROGRAMS_BUCKET.
+
+        Args:
+            radio_program: RadioProgram instance.
+            program_file: File to be stored
+
+        Returns:
+            str: url containing the persisted file.
+        """
+        current_time = datetime.datetime.now()
+        timestamp = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+        file_name = f"{timestamp}_{radio_program.title}"
+        return cls.s3_connector.store(object_key=file_name, object_data=program_file)
+
+    @classmethod
+    def delete_program(cls, file_name: str):
+        """Delete an object persisted in RADIO_PROGRAMS_BUCKET by file_name.
+
+        Args:
+            file_name: File to be deleted.
+        """
+        cls.s3_connector.delete_object(object_key=file_name)
---
+++ src/audio_api/s3/s3_connector.py
@@ -0,0 +1,126 @@
+"""S3Connector class to write and read files from S3."""
+
+import boto3
+from botocore.client import BaseClient
+from botocore.exceptions import ClientError
+
+from audio_api.settings import get_settings
+
+settings = get_settings()
+
+
+def get_s3_client() -> BaseClient:
+    """Return an s3 client for the current AWS S3 configuration.
+
+    Returns:
+        BaseClient: Boto3 client set up for S3.
+    """
+    return boto3.client(
+        "s3",
+        endpoint_url=settings.S3_ENDPOINT_URL,
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    )
+
+
+class S3PersistenceError(Exception):
+    """S3PersistenceError class."""
+
+
+class S3ClientError(Exception):
+    """S3ClientError class."""
+
+
+class S3Connector:
+    """Base S3Connector class."""
+
+    def __init__(self, bucket_name):
+        """Create a new S3Connector for a specific bucket.
+
+        Args:
+            bucket_name (str): S3 bucket name to be used by the S3Connector.
+        """
+        self.s3_client = get_s3_client()
+        self.bucket_name = bucket_name
+
+    def store(self, object_key: str, object_data: bytes) -> str:
+        """
+        Upload an object to the S3 bucket.
+
+        Args:
+            object_key (str): The key (path) of the object in the S3 bucket.
+            object_data (bytes): The data to be stored in the object.
+
+        Raises:
+            S3ClientError: If failed to get response from S3.
+            S3PersistenceError: If failed to persist requested object.
+
+        Returns:
+            str: String containing the url of the uploaded object.
+        """
+        try:
+            response = self.s3_client.put_object(
+                Bucket=self.bucket_name, Key=object_key, Body=object_data
+            )
+        except ClientError as e:
+            raise S3ClientError(f"Failed to get response from S3: {e}")
+
+        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+        if status != 200:
+            raise S3PersistenceError(
+                f"Unsuccessful S3 put_object response. Status: {status}"
+            )
+
+        return f"'https://{self.bucket_name}.s3.amazonaws.com/{object_key}"
+
+    def read_object(self, object_key: str) -> bytes:
+        """Read an object from the S3 bucket.
+
+        Args:
+            object_key (str): The key (path) of the object in the S3 bucket.
+
+        Raises:
+            S3ClientError: If failed to get response from S3.
+            S3PersistenceError: If failed to retrieve object from S3.
+
+        Returns:
+            bytes: The content of the file.
+        """
+        try:
+            response = self.s3_client.get_object(
+                Bucket=self.bucket_name, Key=object_key
+            )
+        except ClientError as e:
+            raise S3ClientError(f"Failed to get response from S3: {e}")
+
+        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+        if status != 200:
+            raise S3PersistenceError(
+                f"Unsuccessful S3 get_object response. Status - {status}"
+            )
+
+        return response.get("Body")
+
+    def delete_object(self, object_key: str):
+        """Delete an object from the S3 bucket.
+
+        Args:
+            object_key (str): The key (path) of the object in the S3 bucket.
+
+        Raises:
+            S3ClientError: If failed to get response from S3.
+            S3PersistenceError: If failed to retrieve object from S3.
+        """
+        # Attempt to delete the object
+        try:
+            response = self.s3_client.delete_object(
+                Bucket=self.bucket_name, Key=object_key
+            )
+        except ClientError as e:
+            raise S3ClientError(f"Failed to get response from S3: {e}")
+
+        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+        if status != 204:
+            raise S3PersistenceError(
+                f"Object deletion was not successful. Status - {status}"
+            )
