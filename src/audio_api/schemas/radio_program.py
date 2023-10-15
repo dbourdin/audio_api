4f76902d398a35b07cfd88b4d2f44ebdf54c061a
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-15 17:40:52 +0200

    Update schemas.

--- src/audio_api/schemas/radio_program.py
+++ src/audio_api/schemas/radio_program.py
@@ -11,6 +11,18 @@ from audio_api.schemas import APISchema
 class BaseRadioProgramSchema(APISchema):
     """Base RadioProgram API Model."""
 
+
+class BaseDbRadioProgramSchema(BaseRadioProgramSchema):
+    """Base RadioProgramSchema with DB created values."""
+
+    program_id: UUID
+    created_at: datetime
+    updated_at: datetime
+
+
+class RadioProgramCreateIn(BaseRadioProgramSchema):
+    """Parameters received in a POST request."""
+
     title: str = Field(example="Shopping 2.0 #1")
     description: str | None = Field(example="Pilot program")
     air_date: date | None
@@ -21,42 +33,31 @@ class BaseRadioProgramSchema(APISchema):
             "37i9dQZF1DWSDoVybeQisg?si=e15a3a65324a4628"
         )
     )
-    url: str | None
-
-
-class RadioProgramSchema(BaseRadioProgramSchema):
-    """RadioProgramSchema with all RadioProgram parameters."""
-
-    program_id: UUID
-    created_at: datetime
-    updated_at: datetime
-
-
-class RadioProgramCreateIn(BaseRadioProgramSchema):
-    """Parameters received in a POST request."""
 
 
 class RadioProgramCreateDB(RadioProgramCreateIn):
     """Model used to create a new record in a POST request."""
 
+    url: str | None
 
-class RadioProgramCreateOut(RadioProgramSchema):
+
+class RadioProgramCreateOut(BaseDbRadioProgramSchema, RadioProgramCreateDB):
     """Parameters returned in a POST request."""
 
 
-class RadioProgramGet(RadioProgramSchema):
+class RadioProgramGet(RadioProgramCreateOut):
     """Parameters returned in a GET request."""
 
 
-class RadioProgramList(RadioProgramSchema):
+class RadioProgramList(RadioProgramCreateOut):
     """Parameters returned in a GET LIST request."""
 
 
-class RadioProgramUpdateIn(BaseRadioProgramSchema):
+class RadioProgramUpdateIn(RadioProgramCreateIn):
     """Parameters received in a PUT request."""
 
 
-class RadioProgramUpdateDB(RadioProgramUpdateIn):
+class RadioProgramUpdateDB(RadioProgramCreateDB):
     """Model used to update a record in a PUT request."""
 
 
