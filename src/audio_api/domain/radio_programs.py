d61db6877239461f2c098511485a80d05022dfd9
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-16 20:38:55 +0200

    Add logic to update RadioPrograms handling exceptions.

--- src/audio_api/domain/radio_programs.py
+++ src/audio_api/domain/radio_programs.py
@@ -7,7 +7,11 @@ from sqlalchemy.orm import Session
 from audio_api import schemas
 from audio_api.persistence.models import RadioProgram
 from audio_api.persistence.repositories import radio_programs_repository
-from audio_api.s3.program_file_persistence import ProgramFilePersistence
+from audio_api.persistence.repositories.radio_program import RadioProgramDatabaseError
+from audio_api.s3.program_file_persistence import (
+    ProgramFilePersistence,
+    RadioProgramS3Error,
+)
 from audio_api.schemas import RadioProgramCreateDB
 
 
@@ -76,3 +80,66 @@ class RadioPrograms:
         # TODO: Extract audio length
         radio_program_db = RadioProgramCreateDB(**radio_program.dict(), url=url)
         return cls.repository.create(db, obj_in=radio_program_db)
+
+    @classmethod
+    def update(
+        cls,
+        *,
+        db: Session,
+        program_id: uuid.UUID,
+        new_program: schemas.RadioProgramUpdateIn,
+        program_file: BinaryIO = None,
+    ) -> RadioProgram:
+        """Update an existing RadioProgram with new properties and new file if included.
+
+        Args:
+            db: A database session.
+            program_id: of the RadioProgram to retrieve.
+            new_program: RadioProgramUpdateIn model with new data.
+            program_file: If there is a program file, it will be uploaded to S3.
+
+        Raises:
+            RadioProgramDatabaseError: If failed to update RadioProgram in DB.
+
+        Returns:
+            RadioProgram: Model containing updated data.
+        """
+        db_program = cls.get(db=db, program_id=program_id)
+        update_program = schemas.RadioProgramUpdateDB(**new_program.dict())
+
+        if program_file:
+            # Will throw RadioProgramS3Error if fails to persist program.
+            url = cls.program_file_persistence.persist_program(
+                radio_program=new_program, program_file=program_file
+            )
+            update_program.url = url
+
+        try:
+            updated_program = radio_programs_repository.update(
+                db=db, db_obj=db_program, obj_in=update_program
+            )
+        except RadioProgramDatabaseError as e:
+            if update_program.url:
+                try:
+                    # Remove uploaded file in s3
+                    cls.program_file_persistence.delete_program_by_url(
+                        update_program.url
+                    )
+                except RadioProgramS3Error:
+                    pass
+                    # TODO: Should we care if we failed to delete?
+                    # TODO: Run a monthly job to cleanup orphan programs?
+                    # This could potentially remove new uploaded programs during cleanup
+            raise e
+
+        if updated_program.url and db_program.url:
+            try:
+                # Remove previous file in s3
+                cls.program_file_persistence.delete_program_by_url(db_program.url)
+            except RadioProgramS3Error:
+                pass
+                # TODO: Should we care if we failed to delete?
+                # TODO: Run a monthly job to cleanup orphan programs?
+                # This could potentially remove new uploaded programs during cleanup
+
+        return updated_program
