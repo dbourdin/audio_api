e6e44cec8f8bd08cb5a239f52a2ba95f31e1a709
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-16 20:26:03 +0200

    Add logic in RadioProgramRepository to raise exception if something goes wrong on update().

--- src/audio_api/persistence/repositories/radio_program.py
+++ src/audio_api/persistence/repositories/radio_program.py
@@ -2,11 +2,16 @@
 
 from uuid import UUID
 
+from sqlalchemy.exc import DatabaseError
 from sqlalchemy.orm import Session
 
 from audio_api.persistence.models.radio_program import RadioProgram
 from audio_api.persistence.repositories.base_repository import BaseRepository
 from audio_api.schemas import RadioProgramCreateDB, RadioProgramUpdateDB
+
+
+class RadioProgramDatabaseError(Exception):
+    """RadioProgramDatabaseError class to handle DB errors."""
 
 
 class RadioProgramRepository(
@@ -25,6 +30,33 @@ class RadioProgramRepository(
             Optional[RadioProgram]: The retrieved radio program
         """
         return db.query(self.model).filter(self.model.program_id == program_id).first()
+
+    def update(
+        self,
+        db: Session,
+        *,
+        db_program: RadioProgram,
+        updated_program: RadioProgramUpdateDB,
+    ) -> RadioProgram:
+        """Update an existing RadioProgram.
+
+        Args:
+            db: A database session
+            db_program: Existing RadioProgram in the DB.
+            updated_program: Updated RadioProgram instance.
+
+        Raises:
+            RadioProgramDatabaseError: If failed to update RadioProgram.
+
+        Returns:
+            RadioProgram: The updated RadioProgram properties.
+        """
+        try:
+            return super().update(db=db, db_obj=db_program, obj_in=updated_program)
+        except DatabaseError as e:
+            raise RadioProgramDatabaseError(
+                f"Failed to update RadioProgram {db_program.program_id}: {e}"
+            )
 
 
 radio_programs_repository = RadioProgramRepository(RadioProgram)
