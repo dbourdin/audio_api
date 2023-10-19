38290b64fc7ac00cfec97c9f529d9761161dbc2f
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-20 01:05:33 +0200

    Add file with dynamodb table config.

---
+++ src/audio_api/aws/dynamodb/tables.py
@@ -0,0 +1,28 @@
+"""Module containing DynamoDb tables and structures."""
+
+from pydantic import BaseModel
+
+from audio_api.aws.settings import DynamoDbTables
+
+
+class DynamoDbTable(BaseModel):
+    """DynamoDbTable class with Table properties."""
+
+    table_name: str
+    attribute_name: str
+    attribute_type: str
+    key_type: str
+    read_capacity_units: int
+    write_capacity_units: int
+
+
+dynamodb_tables = {
+    DynamoDbTables.RadioPrograms: DynamoDbTable(
+        table_name=DynamoDbTables.RadioPrograms,
+        attribute_name="id",
+        attribute_type="S",
+        key_type="HASH",
+        read_capacity_units=5,
+        write_capacity_units=5,
+    )
+}
