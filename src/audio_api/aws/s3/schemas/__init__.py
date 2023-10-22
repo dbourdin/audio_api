1b8bc60f9cdc01db4e16cc3e1c64bd17b74689cd
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-22 18:13:12 +0200

    Fix schemas.

--- src/audio_api/aws/s3/schemas/__init__.py
+++ src/audio_api/aws/s3/schemas/__init__.py
@@ -1,2 +1,6 @@
-from audio_api.aws.s3.schemas.base_schema import S3BaseSchema
+from audio_api.aws.s3.schemas.base_schema import (
+    S3BaseSchema,
+    S3CreateSchema,
+    S3FileSchema,
+)
 from audio_api.aws.s3.schemas.radio_program_file import RadioProgramFileSchema
--- src/audio_api/aws/s3/schemas/base_schema.py
+++ src/audio_api/aws/s3/schemas/base_schema.py
@@ -1,4 +1,5 @@
 """S3BaseSchema class."""
+from typing import BinaryIO
 
 from audio_api.schemas import APISchema
 
@@ -6,5 +7,16 @@ from audio_api.schemas import APISchema
 class S3BaseSchema(APISchema):
     """S3BaseSchema class."""
 
-    file_name: str | None
-    file_url: str | None
+    file_name: str
+
+
+class S3FileSchema(S3BaseSchema):
+    """S3FileSchema class."""
+
+    file_url: str
+
+
+class S3CreateSchema(S3BaseSchema):
+    """S3CreateSchema class."""
+
+    file: BinaryIO
