12ba96b274d6595aca9aa0cf8ba3c39e4187076c
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-22 18:12:33 +0200

    Add s3 exceptions.

---
+++ src/audio_api/aws/s3/exceptions.py
@@ -0,0 +1,13 @@
+"""S3 Exceptions."""
+
+
+class S3PersistenceError(Exception):
+    """S3PersistenceError class."""
+
+
+class S3ClientError(Exception):
+    """S3ClientError class."""
+
+
+class S3BucketNotImplementedError(Exception):
+    """S3BucketNotImplementedError class."""
