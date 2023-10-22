363aafa35cc4198119c89ec0aafb730a0a715950
Damian Bourdin <damian.bourdin@gmail.com>
2023-10-22 19:42:25 +0200

    Add radio_program_files_repository.

---
+++ src/audio_api/aws/s3/repositories/__init__.py
@@ -0,0 +1,3 @@
+from audio_api.aws.s3.repositories.radio_program_files import (
+    radio_program_files_repository,
+)
---
+++ src/audio_api/aws/s3/repositories/radio_program_files.py
@@ -0,0 +1,13 @@
+"""RadioProgramFilesRepository class."""
+
+from audio_api.aws.s3.repositories.base_repository import BaseS3Repository
+from audio_api.aws.s3.schemas import RadioProgramFile, RadioProgramFileCreate
+
+
+class RadioProgramFilesRepository(
+    BaseS3Repository[RadioProgramFile, RadioProgramFileCreate]
+):
+    """RadioProgramFilesRepository class."""
+
+
+radio_program_files_repository = RadioProgramFilesRepository(RadioProgramFile)
