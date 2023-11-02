f13d48cc65889082a4bf41ff1d0b3ed699908b2e
Damian Bourdin <damian.bourdin@gmail.com>
2023-11-02 01:34:42 +0100

    Fix logger config.

--- src/audio_api/logger/logger.py
+++ src/audio_api/logger/logger.py
@@ -5,7 +5,7 @@ import logging
 from audio_api.logger.settings import get_settings
 
 settings = get_settings()
-logging.basicConfig(format=settings.LOG_FORMAT, level=settings.LOG_LEVEL)
+logging.basicConfig(format=settings.LOG_FORMAT, level=settings.LOG_LEVEL.value)
 
 
 def get_logger(logger_name: str) -> logging.Logger:
