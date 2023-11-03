c5c4394d9fe17ef9a62228ab99111649232139f4
Damian Bourdin <damian.bourdin@gmail.com>
2023-11-03 20:18:02 +0100

    Using poetry to run tests.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-# Runs the test suite inside a container
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-function wait_for_db {
-    echo "Waiting db startup..."
-    until docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml exec db pg_isready ; do sleep 1 ; done
-    echo "DB is ready!"
-}
-
-# Ensure all containers are down
-docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml down
-
-# Start environment and wait for DB
-docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml up -d audio-api
-wait_for_db
-
-# Run tests
-docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml exec audio-api pytest --cov="audio_api" $@
-
-# Tear down environment
-docker-compose -f "$SCRIPT_DIR"/../docker/docker-compose.yml down
+poetry run pytest --cov="audio_api"
