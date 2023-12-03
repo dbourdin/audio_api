0c75c26e882d4c8964110fc618fb3059e9505596
Damian Bourdin <damian.bourdin@gmail.com>
2023-12-03 16:43:40 +0100

    Moved create upload_file to test_utils.

--- tests/api/test_utils.py
+++ tests/api/test_utils.py
@@ -1,8 +1,13 @@
 """Test util functions."""
 import uuid
+from pathlib import Path
 from tempfile import SpooledTemporaryFile
 
+from starlette.datastructures import Headers, UploadFile
+
 from audio_api.domain.models import RadioProgramFileModel, RadioProgramModel
+
+MAX_FILE_SIZE = 1024 * 1024
 
 
 def radio_program(title: str) -> RadioProgramModel:
@@ -22,3 +27,35 @@ def create_temp_file() -> dict:
     return {
         "program_file": ("program_file", SpooledTemporaryFile(), "multipart/form-data")
     }
+
+
+def create_upload_file(
+    file: Path,
+) -> tuple[UploadFile, bytes]:
+    """Open a file and creates an UploadFile object.
+
+    Args:
+        file: File to upload.
+
+    Returns:
+        UploadFile: File to upload wrapped in UploadFile model.
+        bytes: File content to be uploaded.
+    """
+    file_name = file.name
+    headers = {
+        "content-disposition": (
+            f'form-data; name="program_file"; filename="{file_name}'
+        ),
+        # TODO: Extract from file?
+        "content-type": "audio/mpeg",
+    }
+    headers = Headers(headers=headers)
+    temp_file = SpooledTemporaryFile(max_size=MAX_FILE_SIZE)
+    upload_file = UploadFile(
+        file=temp_file, size=0, filename=file_name, headers=headers
+    )
+    with open(file, "rb") as f:
+        file_content = f.read()
+    upload_file.file.write(file_content)
+    upload_file.file.seek(0)
+    return upload_file, file_content
--- tests/aws/s3/test_radio_program_files_repository.py
+++ tests/aws/s3/test_radio_program_files_repository.py
@@ -1,25 +1,23 @@
 """Test RadioProgramFilesRepository."""
 import unittest
 from pathlib import Path
-from tempfile import SpooledTemporaryFile
 from unittest import mock
 
 import pytest
 import requests
 from botocore.exceptions import ClientError
-from starlette.datastructures import Headers, UploadFile
 
 from audio_api.aws.s3.exceptions import S3ClientError, S3PersistenceError
 from audio_api.aws.s3.models import S3CreateModel
 from audio_api.aws.s3.repositories import radio_program_files_repository
+from tests.api.test_utils import create_upload_file
 from tests.aws.testcontainers.localstack import localstack_container
 
-TEST_AUDIO_FILE = "test_audio_file.mp3"
+TEST_AUDIO_FILE = Path(__file__).resolve().parent.joinpath("test_audio_file.mp3")
 S3_CLIENT_MOCK_PATCH = (
     "audio_api.aws.s3.repositories.radio_program_files"
     ".radio_program_files_repository.s3_client"
 )
-MAX_FILE_SIZE = 1024 * 1024
 
 
 class TestRadioProgramFilesRepository(unittest.TestCase):
@@ -27,53 +25,17 @@ class TestRadioProgramFilesRepository(unittest.TestCase):
 
     _localstack_container = localstack_container
     _radio_program_files_repository = radio_program_files_repository
-    _test_audio_file = Path(__file__).resolve().parent.joinpath(TEST_AUDIO_FILE)
 
-    def _get_upload_file(
-        self, file: Path = _test_audio_file
-    ) -> tuple[UploadFile, bytes]:
-        """Open a file and creates an UploadFile object.
-
-        Args:
-            file: File to upload.
-
-        Returns:
-            UploadFile: File to upload wrapped in UploadFile model.
-            bytes: File content to be uploaded.
-        """
-        file_name = file.name
-        headers = {
-            "content-disposition": (
-                f'form-data; name="program_file"; filename="{file_name}'
-            ),
-            # TODO: Extract from file?
-            "content-type": "audio/mpeg",
-        }
-        headers = Headers(headers=headers)
-        temp_file = SpooledTemporaryFile(max_size=MAX_FILE_SIZE)
-        upload_file = UploadFile(
-            file=temp_file, size=0, filename=file_name, headers=headers
-        )
-        with open(file, "rb") as f:
-            file_content = f.read()
-        upload_file.file.write(file_content)
-        upload_file.file.seek(0)
-        return upload_file, file_content
-
-    @classmethod
-    def setUpClass(cls):
-        """Set up method to start localstack container before running the tests."""
-        cls._localstack_container.start()
-
-    @classmethod
-    def tearDownClass(cls):
-        """Teardown method to stop localstack container after running the tests."""
-        cls._localstack_container.stop()
+    @pytest.fixture(scope="class", autouse=True)
+    def localstack_container(self):
+        """Start and stop localstack container."""
+        with self._localstack_container:
+            yield self._localstack_container
 
     def test_upload_file_to_s3(self):
         """Test that we can upload a file successfully to S3."""
         # Given
-        upload_file, content = self._get_upload_file()
+        upload_file, content = create_upload_file(TEST_AUDIO_FILE)
         file_name = upload_file.filename.split(".")[0]
         radio_program_create_model = S3CreateModel(
             file_name=file_name,
@@ -95,7 +57,7 @@ class TestRadioProgramFilesRepository(unittest.TestCase):
     def test_upload_file_to_s3_raises_s3_client_error(self, s3_client_mock):
         """Test S3ClientError is raised if put_object raises ClientError."""
         # Given
-        upload_file, content = self._get_upload_file()
+        upload_file, content = create_upload_file(TEST_AUDIO_FILE)
         file_name = upload_file.filename.split(".")[0]
         radio_program_create_model = S3CreateModel(
             file_name=file_name,
@@ -116,7 +78,7 @@ class TestRadioProgramFilesRepository(unittest.TestCase):
     def test_upload_file_to_s3_raises_s3_persistence_error(self, s3_client_mock):
         """Test S3PersistenceError is raised if put_object returns an error code."""
         # Given
-        upload_file, content = self._get_upload_file()
+        upload_file, content = create_upload_file(TEST_AUDIO_FILE)
         file_name = upload_file.filename.split(".")[0]
         radio_program_create_model = S3CreateModel(
             file_name=file_name,
