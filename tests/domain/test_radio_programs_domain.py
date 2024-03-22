16864e32fd5f25c778630b5d93b17fa97f05357f
Damian Bourdin <damian.bourdin@gmail.com>
2024-03-22 11:48:36 +0100

    Add test for create radio program using domain.

--- tests/domain/test_radio_programs_domain.py
+++ tests/domain/test_radio_programs_domain.py
@@ -4,21 +4,53 @@ import unittest
 
 import pytest
 
+from audio_api.api.schemas import RadioProgramCreateInSchema
 from audio_api.aws.dynamodb.models import RadioProgramPutItemModel
 from audio_api.aws.dynamodb.repositories.radio_programs import RadioProgramsRepository
+from audio_api.aws.s3.repositories.radio_program_files import (
+    RadioProgramFilesRepository,
+)
 from audio_api.domain.radio_programs import RadioPrograms
+from tests.api.test_utils import UploadFileModel
 
 
 @pytest.mark.usefixtures("localstack")
 @pytest.mark.usefixtures("radio_programs")
 @pytest.mark.usefixtures("radio_programs_repository")
+@pytest.mark.usefixtures("radio_program_files_repository")
 @pytest.mark.usefixtures("create_program_model")
+@pytest.mark.usefixtures("upload_file")
 class TestRadioProgramsDomain(unittest.TestCase):
     """TestRadioProgramsDomain class."""
 
     radio_programs: RadioPrograms
     radio_programs_repository: RadioProgramsRepository
+    radio_program_files_repository: RadioProgramFilesRepository
     create_program_model: RadioProgramPutItemModel
+    upload_file: UploadFileModel
+
+    def test_create_radio_program(self):
+        """Should create a new RadioProgram."""
+        # Given
+        radio_program_in = RadioProgramCreateInSchema(
+            **self.create_program_model.dict()
+        )
+        radio_program_file = self.upload_file
+
+        # When
+        db_radio_program = self.radio_programs.create(
+            radio_program=radio_program_in, program_file=radio_program_file.file
+        )
+        uploaded_object = self.radio_program_files_repository.get_object(
+            db_radio_program.radio_program.file_name
+        )
+
+        # Then
+        assert db_radio_program.title == radio_program_in.title
+        assert db_radio_program.description == radio_program_in.description
+        assert db_radio_program.air_date == radio_program_in.air_date
+        assert db_radio_program.spotify_playlist == radio_program_in.spotify_playlist
+        assert uploaded_object.read() == radio_program_file.file_content
 
     def test_get_radio_program(self):
         """Should retrieve an existing RadioProgram."""
