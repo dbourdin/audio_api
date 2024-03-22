78d7e6e714ff06f1223af699f66b2e92659c907e
Damian Bourdin <damian.bourdin@gmail.com>
2024-03-22 14:11:24 +0100

    Added tests for update radio program.

--- tests/domain/test_radio_programs_domain.py
+++ tests/domain/test_radio_programs_domain.py
@@ -5,10 +5,11 @@ from unittest import mock
 
 import pytest
 
-from audio_api.api.schemas import RadioProgramCreateInSchema
+from audio_api.api.schemas import RadioProgramCreateInSchema, RadioProgramUpdateInSchema
 from audio_api.aws.dynamodb.exceptions import DynamoDbClientError
 from audio_api.aws.dynamodb.models import RadioProgramPutItemModel
 from audio_api.aws.dynamodb.repositories.radio_programs import RadioProgramsRepository
+from audio_api.aws.s3.exceptions import S3FileNotFoundError
 from audio_api.aws.s3.repositories.radio_program_files import (
     RadioProgramFilesRepository,
 )
@@ -20,6 +21,9 @@ RADIO_PROGRAMS_REPOSITORY_PATH = (
 )
 RADIO_PROGRAMS_REPOSITORY_PUT_ITEM_MOCK_PATCH = (
     f"{RADIO_PROGRAMS_REPOSITORY_PATH}.put_item"
+)
+RADIO_PROGRAMS_REPOSITORY_UPDATE_ITEM_MOCK_PATCH = (
+    f"{RADIO_PROGRAMS_REPOSITORY_PATH}.update_item"
 )
 
 
@@ -37,6 +41,14 @@ class TestRadioProgramsDomain(unittest.TestCase):
     radio_program_files_repository: RadioProgramFilesRepository
     create_program_model: RadioProgramPutItemModel
     upload_file: UploadFileModel
+
+    @pytest.fixture(autouse=True)
+    def _empty_bucket(self):
+        self.radio_program_files_repository.delete_all()
+
+    @pytest.fixture(autouse=True)
+    def _clear_db(self):
+        self.radio_programs_repository.delete_all()
 
     def test_create_radio_program(self):
         """Should create a new RadioProgram."""
@@ -65,7 +77,7 @@ class TestRadioProgramsDomain(unittest.TestCase):
     def test_create_radio_program_raises_dynamo_db_client_error(
         self, put_item_mock: mock.patch
     ):
-        """Should raise DynamoDbClientError if fails to store file in S3."""
+        """Should raise DynamoDbClientError if fails to store object in DynamoDB."""
         # Given
         radio_program_in = RadioProgramCreateInSchema(
             **self.create_program_model.dict()
@@ -114,3 +126,101 @@ class TestRadioProgramsDomain(unittest.TestCase):
         # Then
         assert created_radio_program_1 in db_radio_programs
         assert created_radio_program_2 in db_radio_programs
+
+    def test_update_existing_radio_program(self):
+        """Should update an existing RadioProgram."""
+        # Given
+        radio_program_in = RadioProgramCreateInSchema(
+            **self.create_program_model.dict()
+        )
+        radio_program_file = self.upload_file
+        created_radio_program = self.radio_programs.create(
+            radio_program=radio_program_in, program_file=radio_program_file.file
+        )
+        update_program = RadioProgramUpdateInSchema(title="Updated title")
+
+        # When
+        updated_program = self.radio_programs.update(
+            program_id=created_radio_program.id, new_program=update_program
+        )
+
+        # Then
+        assert updated_program.title == update_program.title
+        assert updated_program.title != created_radio_program.title
+
+    def test_update_existing_radio_program_file(self):
+        """Should update an existing RadioProgram with a new radio_program_file."""
+        # Given
+        radio_program_in = RadioProgramCreateInSchema(
+            **self.create_program_model.dict()
+        )
+        radio_program_file = self.upload_file
+        new_radio_program_file = self.upload_file
+        created_radio_program = self.radio_programs.create(
+            radio_program=radio_program_in, program_file=radio_program_file.file
+        )
+        update_program = RadioProgramUpdateInSchema(title="Updated_title")
+
+        # When
+        updated_program = self.radio_programs.update(
+            program_id=created_radio_program.id,
+            new_program=update_program,
+            program_file=new_radio_program_file.file,
+        )
+        uploaded_object = self.radio_program_files_repository.get_object(
+            updated_program.radio_program.file_name
+        )
+
+        # Then
+        assert update_program.title in updated_program.radio_program.file_name
+        # TODO: Fixme new_radio_program_file
+        assert uploaded_object.read() is True
+        with pytest.raises(S3FileNotFoundError):
+            self.radio_program_files_repository.get_object(
+                created_radio_program.radio_program.file_name
+            )
+
+    @mock.patch(RADIO_PROGRAMS_REPOSITORY_UPDATE_ITEM_MOCK_PATCH)
+    def test_update_radio_program_raises_dynamo_db_client_error(
+        self, update_item_mock: mock.patch
+    ):
+        """Should raise DynamoDbClientError if fails to store object in DynamoDB."""
+        # Given
+        created_radio_program = self.radio_programs_repository.put_item(
+            self.create_program_model
+        )
+        update_program = RadioProgramUpdateInSchema(title="Updated title")
+
+        # When
+        update_item_mock.side_effect = DynamoDbClientError("Test error")
+
+        # Then
+        with pytest.raises(DynamoDbClientError):
+            self.radio_programs.update(
+                program_id=created_radio_program.id, new_program=update_program
+            )
+
+    @mock.patch(RADIO_PROGRAMS_REPOSITORY_UPDATE_ITEM_MOCK_PATCH)
+    def test_failed_update_radio_program_with_program_file_deletes_file(
+        self, update_item_mock: mock.patch
+    ):
+        """Should raise DynamoDbClientError if fails to store object in DynamoDB."""
+        # Given
+        created_radio_program = self.radio_programs_repository.put_item(
+            self.create_program_model
+        )
+        new_file = self.upload_file
+        update_program = RadioProgramUpdateInSchema(title="Updated title")
+
+        # When
+        update_item_mock.side_effect = DynamoDbClientError("Test error")
+
+        # Then
+        with pytest.raises(DynamoDbClientError):
+            self.radio_programs.update(
+                program_id=created_radio_program.id,
+                new_program=update_program,
+                program_file=new_file.file,
+            )
+
+        assert self.radio_program_files_repository.list_objects() == []
